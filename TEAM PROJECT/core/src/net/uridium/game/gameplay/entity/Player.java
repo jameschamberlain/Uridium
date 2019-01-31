70e9f60e90a0282196b64760491130d01199e376
dxh763 <dxh763@student.bham.ac.uk>
2019-01-31 13:25:42 +0000

    - Added some basic assets - Bullet is now a rendered texture (penguin) - Player is now a rendered texture (also penguin) - Player is an InputProcessor and all bullet handling is handled by the the Player class - Player has a reference to level - Level now uses a grid layout, and can load a basic level from a text file - Tile is basically a rectangle in the grid - TempScreen now just renders the level, the player object is now in the Level class - Added an InputProcessor class

--- TEAM PROJECT/core/src/net/uridium/game/gameplay/entity/Player.java
+++ TEAM PROJECT/core/src/net/uridium/game/gameplay/entity/Player.java
@@ -3,40 +3,47 @@ package net.uridium.game.gameplay.entity;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
+import net.uridium.game.gameplay.Level;
+import net.uridium.game.util.UridiumInputProcessor;
 
-public class Player {
+public class Player extends UridiumInputProcessor {
     final Rectangle body;
     Color c = new Color(10673872);
     Color c2 = new Color();
 
     float moveSpeed = 300;
+    Level level;
 
     public Vector2 lastPos;
     public long lastShot = 0;
-    private long reloadTime = 200;
+    private long reloadTime = 350;
 
-    public Player(float x, float y, float width, float height) {
+    Texture texture;
+
+    public Player(float x, float y, float width, float height, Level level) {
         lastPos = new Vector2(x, y);
         body = new Rectangle(x, y, width, height);
+
+        this.level = level;
+        texture = new Texture(Gdx.files.internal("penguin_square.png"));
     }
 
     public Rectangle getBody() {
         return body;
     }
 
-    public void shoot() {
+    private void shoot(Vector2 bulletSpawn, float shootAngle) {
+        level.spawnBullet(new Bullet(bulletSpawn, shootAngle));
         lastShot = System.currentTimeMillis();
     }
 
-    public boolean canShoot() {
+    private boolean canShoot() {
         return System.currentTimeMillis() - lastShot > reloadTime;
-    }
-
-    public Vector2 getCenter() {
-        return new Vector2(body.x + body.width / 2, body.y + body.height / 2);
     }
 
     public void goToLastXPos() {
@@ -54,8 +61,6 @@ public class Player {
         if(Gdx.input.isKeyPressed(Input.Keys.W) || Gdx.input.isKeyPressed(Input.Keys.A) || Gdx.input.isKeyPressed(Input.Keys.S) || Gdx.input.isKeyPressed(Input.Keys.D))
             moved = true;
 
-        if(moved) System.out.println(body.y);
-
         if(Gdx.input.isKeyPressed(Input.Keys.W))
             body.y += moveSpeed * delta;
         if(Gdx.input.isKeyPressed(Input.Keys.A))
@@ -65,16 +70,39 @@ public class Player {
         if(Gdx.input.isKeyPressed(Input.Keys.D))
             body.x += moveSpeed * delta;
 
-        if(moved) {
-            System.out.println(body.y);
-            System.out.println("--");
-        }
     }
 
-    public void render(ShapeRenderer shapeRenderer) {
-        shapeRenderer.setColor(c2);
-        shapeRenderer.rect(body.x, body.y, body.width, body.height);
-        shapeRenderer.setColor(c);
-        shapeRenderer.rect(body.x + 4, body.y + 4, body.width - 8, body.height - 8);
+    public void render(SpriteBatch batch) {
+        batch.draw(texture, body.x, body.y, body.width, body.height);
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        Vector2 bulletSpawn = new Vector2();
+
+        if(canShoot()) {
+            switch(keycode) {
+                case Input.Keys.UP:
+                    bulletSpawn = body.getCenter(bulletSpawn).add(0, body.height / 2);
+                    shoot(bulletSpawn, 0);
+                    return true;
+                case Input.Keys.LEFT:
+                    bulletSpawn = body.getCenter(bulletSpawn).sub(body.width / 2, 0);
+                    shoot(bulletSpawn, 270);
+                    return true;
+                case Input.Keys.DOWN:
+                    bulletSpawn = body.getCenter(bulletSpawn).sub(0, body.height / 2);
+                    shoot(bulletSpawn, 180);
+                    return true;
+                case Input.Keys.RIGHT:
+                    bulletSpawn = body.getCenter(bulletSpawn).add(body.width / 2, 0);
+                    shoot(bulletSpawn, 90);
+                    return true;
+            }
+        }
+
+        return super.keyDown(keycode);
     }
 }
+
+
