18065d940eb5f0a1f33397ed6eedfaabf7648ab8
jxc1089 <jxc1089@student.bham.ac.uk>
2019-02-18 17:11:50 +0000

    Not sure what I'm committing, but git wants me to

--- Pathfinding Test/src/Main.java
+++ Pathfinding Test/src/Main.java
@@ -13,13 +13,16 @@ public class Main {
 
         long startTime = System.currentTimeMillis();
 
-        Tuple gridSize = new Tuple(5, 5);
+        Tuple gridSize = new Tuple(20, 20);
         Point start = new Point(1, 1);
-        Point end = new Point(4, 4);
+        Point end = new Point(18, 17);
         ArrayList<Point> obstacles = new ArrayList<>();
         obstacles.add(new Point(2, 2));
         obstacles.add(new Point(3, 3));
         obstacles.add(new Point(4, 3));
+        obstacles.add(new Point(12, 12));
+        obstacles.add(new Point(13, 13));
+        obstacles.add(new Point(14, 13));
         Pathfinder pathfinder = new Pathfinder(gridSize, start, end, obstacles);
         ArrayList<Object> route = pathfinder.findPath();
         System.out.println(route);
