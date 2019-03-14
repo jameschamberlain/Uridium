55d84d2d3cbf3bee88af4f1f4f04fdcbf0909bad
jxc1089 <jxc1089@student.bham.ac.uk>
2019-03-14 22:25:53 +0000

    Created lobby screen

--- TEAM PROJECT/core/src/net/uridium/game/screen/LobbyScreen.java
+++ TEAM PROJECT/core/src/net/uridium/game/screen/LobbyScreen.java
@@ -1,103 +1,101 @@
 package net.uridium.game.screen;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.assets.AssetManager;
-import com.badlogic.gdx.audio.Music;
-import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL30;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.math.Interpolation;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Button;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.viewport.FitViewport;
-import net.uridium.game.Uridium;
-import net.uridium.game.util.GameConstants;
 import net.uridium.game.util.MyAssetManager;
 
-import static com.badlogic.gdx.scenes.scene2d.actions.Actions.*;
 import static net.uridium.game.Uridium.*;
 import static net.uridium.game.screen.UridiumScreenManager.getUSMInstance;
 
-public class MenuScreen extends UridiumScreen {
+public class LobbyScreen extends UridiumScreen {
 
     private OrthographicCamera camera;
     private SpriteBatch batch;
 
     private Skin mySkin;
     private Stage stage;
+    private String input;
 
     Texture bgTexture;
     TextureRegion bg;
 
+    // ROOM CODE!
+    public String code = "";
 
-    public MenuScreen(){
+
+    public LobbyScreen() {
         setCursor("cursor.png", 0, 0);
 
+        // Setup textures and background.
         bgTexture = new Texture(Gdx.files.internal("ground_01.png"));
         bgTexture.setWrap(Texture.TextureWrap.Repeat, Texture.TextureWrap.Repeat);
         bg = new TextureRegion(bgTexture);
         bg.setRegion(0, 0, 640, 640);
 
+        // Setup asset manager.
         MyAssetManager myAssetManager = new MyAssetManager();
         myAssetManager.queueAddSkin();
         myAssetManager.manager.finishLoading();
         mySkin = myAssetManager.manager.get("skin/glassy-ui.json");
 
+        // Setup camera.
         camera = new OrthographicCamera();
         camera.setToOrtho(false, GAME_WIDTH, GAME_HEIGHT);
 
         batch = new SpriteBatch();
 
+        // Setup stage
         stage = new Stage(new FitViewport(GAME_WIDTH, GAME_HEIGHT, camera), batch);
         Gdx.input.setInputProcessor(stage);
 
 
-        Label gameTitle = new Label("U R I D I U M",mySkin,"big");
+        // Setup window.
+        Label gameTitle = new Label("U R I D I U M", mySkin, "big");
         gameTitle.setSize(1280, 360);
         gameTitle.setPosition(0, 360);
         gameTitle.setFontScale(1.4f);
         gameTitle.setAlignment(Align.center);
 
-        // Setup start button.
-        Button startBtn = new TextButton("P L A Y",mySkin,"small");
-        startBtn.setSize(340,80);
-        startBtn.setPosition((GAME_WIDTH - 340) / 2,(GAME_HEIGHT - 80) / 2);
-        ((TextButton) startBtn).getLabel().setFontScale(1.4f);
-//        startBtn.addAction(sequence(alpha(0), parallel(fadeIn(.5f), moveBy(0, -20, .5f, Interpolation.pow5Out))));
+        // Setup solo button.
+        Button soloBtn = new TextButton("S O L O", mySkin, "small");
+        soloBtn.setSize(340, 80);
+        soloBtn.setPosition((GAME_WIDTH - 340) / 2.0f, (GAME_HEIGHT - 80) / 2.0f);
+        ((TextButton) soloBtn).getLabel().setFontScale(1.4f);
+//        soloBtn.addAction(sequence(alpha(0), parallel(fadeIn(.5f), moveBy(0, -20, .5f, Interpolation.pow5Out))));
         // Listener for start button.
-        startBtn.addListener(new InputListener(){
+        soloBtn.addListener(new InputListener() {
             @Override
             public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-                System.out.println("hi");
                 return true;
             }
 
             @Override
             public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
-                System.out.println("hello");
-                getUSMInstance().push(new LobbyScreen());
+                getUSMInstance().push(new GameScreen());
                 super.touchDown(event, x, y, pointer, button);
             }
         });
 
-        // Setup settings button.
-        Button settingsBtn = new TextButton("S E T T I N G S",mySkin,"small");
-        settingsBtn.setSize(340, 80);
-        settingsBtn.setPosition((GAME_WIDTH - 340) / 2,(GAME_HEIGHT - 80) / 2 - (80 + 20));
-        ((TextButton) settingsBtn).getLabel().setFontScale(1.4f);
-//        settingsBtn.addAction(sequence(alpha(0), parallel(fadeIn(.5f), moveBy(0, -20, .5f, Interpolation.pow5Out))));
-        // Listener for settings button.
-        settingsBtn.addListener(new InputListener(){
+        // Setup multiplayer button.
+        Button multiplayerButton = new TextButton("M U L T I P L A Y E R", mySkin, "small");
+        multiplayerButton.setSize(340, 80);
+        multiplayerButton.setPosition((GAME_WIDTH - 340) / 2.0f, (GAME_HEIGHT - 80) / 2.0f - (80 + 20));
+        ((TextButton) multiplayerButton).getLabel().setFontScale(1.4f);
+//        multiplayerButton.addAction(sequence(alpha(0), parallel(fadeIn(.5f), moveBy(0, -20, .5f, Interpolation.pow5Out))));
+        // Listener for multiplayer button.
+        multiplayerButton.addListener(new InputListener() {
             @Override
             public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
                 return true;
@@ -105,35 +103,43 @@ public class MenuScreen extends UridiumScreen {
 
             @Override
             public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
-                getUSMInstance().push(new SettingsScreen());
+
+                Label label = new Label("Enter room ID", mySkin);
+                label.setAlignment(Align.center);
+                label.setColor(Color.BLACK);
+
+                TextField textField = new TextField("", mySkin);
+
+                Button confirmButton = new TextButton("C O N F I R M", mySkin, "small");
+
+                Button cancelButton = new TextButton("C A N C E L", mySkin, "small");
+
+
+                // Setup dialog to ask for room code
+                Dialog dialog = new Dialog("Room code", mySkin) {
+                    protected void result(Object object) {
+                        if (object.equals(true) && !(textField.getText().isEmpty())) {
+                            code = textField.getText();
+                            System.out.println(code);
+                        }
+                    }
+                };
+                dialog.getContentTable().add(label);
+                dialog.getContentTable().add(textField);
+                dialog.key(Input.Keys.ENTER, true).key(Input.Keys.ESCAPE, false);
+                dialog.button(confirmButton, true);
+                dialog.button(cancelButton, false);
+                dialog.setSize(1000, GAME_HEIGHT / 2.0f);
+                dialog.show(stage);
+
                 super.touchUp(event, x, y, pointer, button);
             }
         });
 
-        // Setup exit button.
-        Button exitBtn = new TextButton("E X I T", mySkin, "small");
-        exitBtn.setSize(340, 80);
-        exitBtn.setPosition((GAME_WIDTH - 340) / 2,(GAME_HEIGHT - 80) / 2 - (80 + 20) * 2);
-        ((TextButton) exitBtn).getLabel().setFontScale(1.4f);
-//        exitBtn.addAction(sequence(alpha(0), parallel(fadeIn(.5f), moveBy(0, -20, .5f, Interpolation.pow5Out))));
-        // Listener for exit button.
-        exitBtn.addListener(new InputListener(){
-            @Override
-            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-                return true;
-            }
-
-            @Override
-            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
-                Gdx.app.exit();
-                super.touchUp(event, x, y, pointer, button);
-            }
-        });
 
         stage.addActor(gameTitle);
-        stage.addActor(startBtn);
-        stage.addActor(settingsBtn);
-        stage.addActor(exitBtn);
+        stage.addActor(soloBtn);
+        stage.addActor(multiplayerButton);
     }
 
     @Override
@@ -148,7 +154,7 @@ public class MenuScreen extends UridiumScreen {
 
     @Override
     public void render() {
-        Gdx.gl.glClearColor(1,0,0,0);
+        //Gdx.gl.glClearColor(1,0,0,0);
         Gdx.gl.glClear(GL30.GL_COLOR_BUFFER_BIT);
 
         batch.setProjectionMatrix(camera.combined);
