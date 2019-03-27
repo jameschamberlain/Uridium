494db6bf5f506c002b9f16be77addafb8abff24d
jxs1292 <jxs1292@student.bham.ac.uk>
2019-03-27 22:30:38 +0800

    server created by clicking "Create" button

--- TEAM PROJECT/core/src/net/uridium/game/server/constant.java
+++ TEAM PROJECT/core/src/net/uridium/game/server/constant.java
@@ -4,6 +4,6 @@ package net.uridium.game.server;
  * The class to store network data
  */
 public class constant {
-    public static final String SERVER_IP = "192.168.137.1";
+    public static final String SERVER_IP = "192.168.160.1";
     public static final int LOBBY_SERVER_PORT = 9966;
 }
